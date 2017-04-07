c18e57b88d5da16075c8a7ca58e1eb23d9b9f841
Felix Yin <lafenglafenghaha@163.com>
2017-04-07 15:01:42 +0800

    pod init 2

---
+++ repotestdemo/repotestdemo/Views/PictureView.h
@@ -0,0 +1,13 @@
+//
+//  PictureView.h
+//  repotestdemo
+//
+//  Created by Felix Yin on 2017/4/7.
+//  Copyright © 2017年 Felix Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PictureView : UIView
+
+@end
---
+++ repotestdemo/repotestdemo/Views/PictureView.m
@@ -0,0 +1,21 @@
+//
+//  PictureView.m
+//  repotestdemo
+//
+//  Created by Felix Yin on 2017/4/7.
+//  Copyright © 2017年 Felix Yin. All rights reserved.
+//
+
+#import "PictureView.h"
+
+@implementation PictureView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
