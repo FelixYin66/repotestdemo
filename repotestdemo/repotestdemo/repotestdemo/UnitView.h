18bb0c7723fd9cfbc25daf93407c3ca4ccd8480b
Felix Yin <lafenglafenghaha@163.com>
2017-04-07 14:54:56 +0800

    pod init

---
+++ repotestdemo/repotestdemo/repotestdemo/UnitView.h
@@ -0,0 +1,13 @@
+//
+//  UnitView.h
+//  repotestdemo
+//
+//  Created by Felix Yin on 2017/4/7.
+//  Copyright © 2017年 Felix Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UnitView : UIView
+
+@end
---
+++ repotestdemo/repotestdemo/repotestdemo/UnitView.m
@@ -0,0 +1,21 @@
+//
+//  UnitView.m
+//  repotestdemo
+//
+//  Created by Felix Yin on 2017/4/7.
+//  Copyright © 2017年 Felix Yin. All rights reserved.
+//
+
+#import "UnitView.h"
+
+@implementation UnitView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
